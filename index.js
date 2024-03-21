73422fa2a2d552c42301bec578807e6bbddbe66b
AmonCheruiyot <161847144+AmonCheruiyot@users.noreply.github.com>
2024-03-21 20:55:21 +0300

    completes test

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution here!
+function destructivelyAppendCat(name) { cats.push(name);
+}
+function destructivelyPrependCat(name) { cats.unshift(name);
+}
+function destructivelyRemoveFirstCat() { cats.shift();
+}
+function destructivelyRemoveLastCat() { cats.pop();
+}
+function appendCat(name) { return cats.concat(name);
+}
+function prependCat(name) { return [name, ...cats];
+}
+function removeLastCat() { return cats.slice(0, -1);
+}
+function removeFirstCat() { return cats.slice(1);
+}
